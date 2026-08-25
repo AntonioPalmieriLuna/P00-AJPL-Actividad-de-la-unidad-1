6673b0a583a758ea0671f8812e85ef29f001352d
AntonioPalmieriLuna <apalmieril@unicartagena.edu.co>
2026-08-24 22:44:13 -0500

    Parte-C ejercicio14

---
+++ Parte-C/src/Ejercicio14/CuentaBancaria.java
@@ -0,0 +1,57 @@
+package Ejercicio14;
+
+public class CuentaBancaria {
+    private long numero;
+    private String titular;
+    private int saldo;
+
+    public CuentaBancaria(long numero, String titular, int saldo){
+        this.numero=numero;
+        this.titular=titular;
+        this.saldo=saldo;
+    }
+
+    public void consignar(double cantidad){
+        this.saldo+=cantidad;
+    }
+
+    public void retirar(double cantidad){
+        if(saldo>=cantidad){
+            this.saldo-=cantidad;
+        }else{
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public void mostrarEstado(){
+        System.out.println("Número de cuenta: "+numero+"\n" +
+                "Titular: "+titular+"\n" +
+                "Saldo: "+saldo);
+    }
+
+    public static void main(String[]args){
+        CuentaBancaria cuenta=new CuentaBancaria(12345678998655L,"Juan Pablo",42000);
+        cuenta.mostrarEstado();
+
+        System.out.println();
+
+        //consignar y mostrar nuevo estado
+        cuenta.consignar(100000);
+        cuenta.mostrarEstado();
+
+        System.out.println();
+
+        //retirar y mostrar nuevo estado
+        cuenta.retirar(12000);
+        cuenta.mostrarEstado();
+
+        System.out.println();
+
+        //verificar si da error al sacar una cantidad mayor al saldo
+        cuenta.retirar(10000000);
+
+        System.out.println();
+
+        cuenta.mostrarEstado();
+    }
+}
